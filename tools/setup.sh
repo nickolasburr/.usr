f03f78afd953c6730534cbd861729247cb913966
Nickolas Burr <nickolasburr@gmail.com>
2018-02-06 11:41:37 -0500

    Remove mkln, dotusr functions, run tasks in body

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,92 +1,103 @@
 #!/usr/bin/env bash
+
+set -x
 shopt -s nullglob
 
-# Make symlink from arguments
-mkln () {
-	# Verify source inode exists; if not, notify the user and exit
-	if [[ ! -e "$1" ]]; then
-		printf 'Cannot create symlink from %s, it is not a valid inode\n' "$1"
-		exit 1
-	fi
+[[ -z "$DOTUSR" ]] && export DOTUSR="$HOME/.usr"
 
-	# Create symlink, source -> target
-	ln -fns "$1" "$2"
-}
-
-# Symlink dotfiles stored in ~/.usr/** -> ~
-dotusr () {
-	# Verify this script is ran inside ~/.usr
-	if [[ ! "$PWD" -ef "$HOME/.usr"  ]]; then
-		printf 'This script must be ran inside ~/.usr\n'
-		exit 1
-	fi
-
-	# Group type (defaults -> all, macos, linux)
-	local GROUPTYPE="$1"
-
-	cd bin
-
-	# ~/.usr/bin executables
-	for EXEC in $GROUPTYPE/{.[^.,gitkeep],}*; do
-		local EXECNAME="$(basename $EXEC)"
-
-		mkln "$EXEC" "$EXECNAME"
-	done
-
-	cd ../etc
-
-	# ~/.usr/etc user configs
-	for CONF in $GROUPTYPE/{.[^.,gitkeep],}*; do
-		local BASENAME="$(basename $CONF)"
-
-		# If $BASENAME doesn't have a dot prefix, prepend one
-		if [[ ${BASENAME:0:1} != "." ]]; then
-			BASENAME=".$BASENAME"
-		fi
-
-		# If a directory or file with the same name already exists in $HOME,
-		# ask the user whether they want to remove it entirely or skip it
-		if [[ -e "$HOME/$BASENAME" && ! -L "$HOME/$BASENAME" ]]; then
-			read -p "A directory or file with the name $BASENAME was found in your home directory. Do you want to remove it? [yN]: " ANSWER
-			case $ANSWER in
-				[yY]*)
-					rm -rf "$HOME/$BASENAME"
-					mkln "$DOTUSR/etc/$CONF" "$HOME/$BASENAME"
-				;;
-				[nN]*)
-					echo "Okay, skipping $BASENAME..."
-					continue
-				;;
-				*)
-					echo "Invalid response. Skipping $BASENAME..."
-					continue
-				;;
-			esac
-		else
-			mkln "$DOTUSR/etc/$CONF" "$HOME/$BASENAME"
-		fi
-	done
-
-	cd ..
-}
-
-if [[ -z "$DOTUSR" ]]; then
-	export DOTUSR="$HOME/.usr"
-fi
+ETCDIR="$DOTUSR/etc"
 
 while [[ $# -gt 0 ]]; do
 	case "$1" in
 		-h|--help)
-			printf 'Usage: ./setup.sh {-t|--type} group\n'
+			printf 'Usage: ./setup.sh {-t|--type} <group>\n'
+
 			exit 0
 		;;
 		-t|--type)
 			shift
-			dotusr "$1"
+
+			if [[ ! "$PWD" -ef "$DOTUSR"  ]]; then
+				cd "$DOTUSR"
+			fi
+
+			GROUP="$1"
+
+			cd bin
+
+			# ~/.usr/bin executables
+			for EXEC in $GROUP/{.[^.,gitkeep],}*; do
+				EXECNAME="$(basename $EXEC)"
+
+				if [[ ! -e "$EXEC" ]]; then
+					printf 'Cannot create symlink from source %s, it is not a valid file or directory.\n' "$EXEC"
+
+					exit 1
+				fi
+
+				# Create symlink, source -> target
+				ln -fns "$EXEC" "$EXECNAME"
+			done
+
+			cd ../etc
+
+			# ~/.usr/etc configuration files.
+			for CONF in $GROUP/{.[^.,gitkeep],}*; do
+				BASENAME="$(basename $CONF)"
+
+				# If $BASENAME doesn't have a dot prefix, prepend one.
+				if [[ ${BASENAME:0:1} != "." ]]; then
+					BASENAME=".$BASENAME"
+				fi
+
+				# If a directory or file with the same name already exists in $HOME,
+				# ask the user whether they want to remove it entirely or skip it.
+				if [[ -e "$HOME/$BASENAME" && ! -L "$HOME/$BASENAME" ]]; then
+					read -p "~/$BASENAME already exists. Do you want to remove it? [yN]: " ANSWER
+
+					case $ANSWER in
+						[yY]*)
+							rm -rf "$HOME/$BASENAME"
+
+							if [[ ! -e "$ETCDIR/$CONF" ]]; then
+								printf 'Cannot create symlink from source %s, it is not a valid file or directory.\n' "$ETCDIR/$CONF"
+
+								exit 1
+							fi
+
+							# Create symlink, source -> target
+							ln -fns "$ETCDIR/$CONF" "$HOME/$BASENAME"
+						;;
+						[nN]*)
+							echo "Okay, skipping $BASENAME..."
+
+							continue
+						;;
+						*)
+							echo "Invalid response. Skipping $BASENAME..."
+
+							continue
+						;;
+					esac
+				else
+					if [[ ! -e "$ETCDIR/$CONF" ]]; then
+						printf 'Cannot create symlink from source %s, it is not a valid file or directory.\n' "$ETCDIR/$CONF"
+
+						exit 1
+					fi
+
+					# Create symlink, source -> target
+					ln -fns "$ETCDIR/$CONF" "$HOME/$BASENAME"
+				fi
+			done
+
+			cd ..
+
 			shift
 		;;
 		*)
-			printf 'Invalid option "%s"\n\nUsage: ./setup.sh {-t|--type} group\n' "$1"
+			printf 'Invalid option %s\n\nUsage: ./setup.sh {-t|--type} <group>\n' "$1"
+
 			exit 0
 		;;
 	esac
